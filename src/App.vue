c2e963f21c0a73357a177b8afd8c245e4dac7c1e
LaupWing <loc_strict@hotmail.com>
2020-09-23 10:43:15 +0200

    setup vuex

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,40 @@
 	</div>
 </template>
 <script>
+import faker from 'faker'
 export default {
 	name: 'App',
 	created(){
-		// this.$router.push({name: 'Checkout'})
+		const emptyArray = [...Array(5).keys()]
+		const products = emptyArray
+			.map(x=>{
+				const name = faker.commerce.productName()
+				const image = faker.image.image() 
+				const description = faker.commerce.productDescription() 
+				const price = faker.commerce.price()
+				
+				return {
+					name,
+					description,
+					price,
+					image,
+				}
+			})
+		this.$store.commit('SET_ITEM', {type: 'shoppingcart', value: products})
 	}
 }
 </script>
 <style>
 :root{
 	--highlight-color: orange;
+}
+*{
+	margin: 0;
+	padding: 0;
+}
+#app{
+	display: flex;
+	justify-content: center;
+	width: 100vw;
 }
 </style>
--- src/store/modules/checkout.js
+++ src/store/modules/checkout.js
@@ -1,13 +1,19 @@
 const state = {
-
+	shoppingcart:[]
 }
 const mutations = {
-	
+	'SET_SHOPPING'(state, items){
+		state.shoppingcart = items
+	},
+	'SET_ITEM'(state, {type, value}){
+		state[type] = value
+	}
 }
 const actions ={
    
 }
 const getters = {
+	shoppingcart: state => state.shoppingcart
 }
 export default {
     state,
