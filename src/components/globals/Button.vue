4ae06b6fc644e78911979f3fd8b5cd5b22ff7adc
LaupWing <loc_strict@hotmail.com>
2020-09-23 10:14:03 +0200

    buttons

---
+++ src/components/globals/Button.vue
@@ -0,0 +1,21 @@
+<template>
+	<button>
+		{{text}}
+	</button>
+</template>
+
+<script>
+export default {
+	name: 'Button',
+	props:{
+		text:{
+			type: String,
+			required: true
+		}
+	}
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Checkout/Checkout.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="checkout">
+		<router-view/>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "Checkout",
+	props:{
+		products:{
+			type: Array
+		}
+	},
+	data(){
+		return{
+
+		}
+	}
+}
+</script>
+
+<style>
+
+</style>
